861763c80517b864d0838a4d9c0aa9532b1776f0
Firas SAHLI <firas.sahli@aymax.fr>
2023-07-05 10:56:54 +0200

    initial commit containing springboot application

---
+++ src/test/java/com/sg/bankaccountkata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.bankaccountkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
